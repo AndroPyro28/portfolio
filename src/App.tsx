2e306b7655e2bd8caa101e729f01e41b6d859ebe
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-08-24 02:01:24 +0800

    main

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
 
       </div>
       <Works />
-      <Feedbacks />
+      {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact />
         <StarsCanvas />
